f19710422b873641fa57ca65f6bf10d226bac2ed
gmarinojr <garymarinojr@gmail.com>
2019-09-17 13:33:17 -0500

    updated key to userId

--- server/models/postCount.js
+++ server/models/postCount.js
@@ -1,13 +1,13 @@
 module.exports = function (sequelize, Sequelize) {
   const PostCount = sequelize.define('postCount', {
     count: { type: Sequelize.INTEGER, default: '0' },
-    user_id: {
+    userId: {
       type: Sequelize.INTEGER,
       references: {
         // This is a reference to another model
         model: 'Users',
         // This is the column name of the referenced model
-        key: 'id',
+        key: 'userId',
       },
     },
   });
