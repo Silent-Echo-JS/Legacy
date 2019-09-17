01385f95958fe911722af9787a1ad27d23827d5f
gmarinojr <garymarinojr@gmail.com>
2019-09-17 14:20:27 -0500

    removed db name for sequelize db

--- server/models/index.js
+++ server/models/index.js
@@ -10,22 +10,20 @@ const sequelize = new Sequelize(
   config.password,
   config,
 );
-const db = {};
 
 fs.readdirSync(__dirname)
   .filter((file) => file.indexOf('.') !== 0 && file !== 'index.js')
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
+    sequelize[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
-  if ('associate' in db[modelName]) {
-    db[modelName].associate(db);
+Object.keys(sequelize).forEach((modelName) => {
+  if ('associate' in sequelize[modelName]) {
+    sequelize[modelName].associate(sequelize);
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+sequelize.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = sequelize;
