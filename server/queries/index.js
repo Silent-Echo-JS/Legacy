b625f5bf8e31ba608390f163f9037f492f420bdb
gmarinojr <garymarinojr@gmail.com>
2019-09-17 14:20:56 -0500

    removed db name for sequelize

--- server/queries/index.js
+++ server/queries/index.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary').v2;// api for dealing with image DB, cloudinary
 const config = require('../config/cloudinary');
-const { db, Users, Posts } = require('../models');
+const sequelize = require('../models');
 
 // IF TRYING TO FIND A USER, LOOK AT GETUSER BELOW, decide which to use!!!!!!!!
 // Both find users, from the DB and take all the info from them, find users actually passes the info in the .catch, while get users from the .then
@@ -9,7 +9,7 @@ const findUser = (user) => new Promise((resolve, reject) => {
   // select user from database if exists
   const foundUser = `SELECT * FROM users where username= "${user}"`;
 
-  db.sequelize.query(foundUser, [user], (err, results, fields) => {
+  sequelize.query(foundUser, [user], (err, results, fields) => {
     if (results.length > 0) {
       return reject(user);
     }
@@ -22,7 +22,7 @@ const getUser = (user) => new Promise((resolve, reject) => {
   // select user from database if exists
   const foundUser = `SELECT * FROM users where username= "${user}"`;
 
-  db.sequelize.query(foundUser, [user], (err, results, fields) => {
+  sequelize.query(foundUser, [user], (err, results, fields) => {
     if (results.length > 0) {
       return resolve(results);
     }
@@ -35,7 +35,7 @@ const saveUser = (user) => new Promise((resolve, reject) => {
   const userInsert = 'INSERT INTO users(userId, username, password, email, business) VALUES (DEFAULT, ?)';
   const insertValues = [user.username, user.password, user.email, user.business];
 
-  db.sequelize.query(userInsert, [insertValues], (err, results, fields) => {
+  sequelize.query(userInsert, [insertValues], (err, results, fields) => {
     if (err) {
       console.log(err);
       return reject(err);
@@ -48,7 +48,7 @@ const saveUser = (user) => new Promise((resolve, reject) => {
 const saveUsersPostCount = (userId) => new Promise((resolve, reject) => {
   const countInsert = 'INSERT INTO postCount(count, userId) VALUES (DEFAULT, ?)';
 
-  db.sequelize.query(countInsert, [userId], (err, results) => {
+  sequelize.query(countInsert, [userId], (err, results) => {
     if (err) {
       console.log(err);
       return reject(err);
@@ -61,7 +61,7 @@ const savePost = (post) => new Promise((resolve, reject) => {
   const postInsert = 'INSERT INTO posts(postId, text, img1, title, location, tagList, lumber, metal, concrete, glass, piping, userId) VALUES (DEFAULT, ?)';
   const insertValues = [post.text, post.img1, post.title, post.location, post.tagList, post.lumber, post.metal, post.concrete, post.glass, post.piping, post.userId];
 
-  db.sequelize.query(postInsert, [insertValues], (err, results) => {
+  sequelize.query(postInsert, [insertValues], (err, results) => {
     if (err) {
       console.log(err);
       return reject(err);
@@ -73,7 +73,7 @@ const savePost = (post) => new Promise((resolve, reject) => {
 const increasePostCount = (userId) => new Promise((resolve, reject) => {
   const increaseInsert = 'UPDATE postCount SET count = count + 1 WHERE userId = ?';
 
-  db.sequelize.query(increaseInsert, [userId], (err, results) => {
+  sequelize.query(increaseInsert, [userId], (err, results) => {
     if (err) {
       console.log(err);
       return reject(err);
@@ -83,15 +83,14 @@ const increasePostCount = (userId) => new Promise((resolve, reject) => {
 });
 
 const displayPosts = () => new Promise((resolve, reject) => {
-  const fetchedPosts = Posts.findAll({ include: [{ model: Users, required: true }] });
-  db.sequelize.query(fetchedPosts, (err, results) => {
+  const fetchedPosts = 'select posts.*, users.userId from posts INNER JOIN users WHERE posts.userId = users.userId';
+  sequelize.query(fetchedPosts, (err, results) => {
     if (err) {
       return reject(err);
     }
     return resolve(results);
   });
 });
-
 
 cloudinary.config(config);// config object for connecting to cloudinary
 
@@ -100,7 +99,7 @@ const saveImage = (image) => cloudinary.uploader.upload(image.tempFilePath);
 // allows you to select all the posts based upon hat type of material you wish
 const searchTags = (tag) => new Promise((resolve, reject) => {
   const searchedTag = `SELECT * FROM posts WHERE ${tag.material} IS TRUE`;
-  db.sequelize.query(searchedTag, (err, results) => {
+  sequelize.query(searchedTag, (err, results) => {
     if (err) {
       return reject(err);
     }
@@ -111,7 +110,7 @@ const searchTags = (tag) => new Promise((resolve, reject) => {
 const getPostInfo = (userId) => new Promise((resolve, reject) => {
   const userIdInsert = 'SELECT users.username, users.email, users.business FROM users WHERE userId = ?';
 
-  db.sequelize.query(userIdInsert, [userId], (err, results) => {
+  sequelize.query(userIdInsert, [userId], (err, results) => {
     if (err) {
       console.log(err);
       return reject(err);
@@ -123,7 +122,7 @@ const getPostInfo = (userId) => new Promise((resolve, reject) => {
 module.exports = {
   findUser,
   getUser,
-  db,
+  sequelize,
   saveUser,
   savePost,
   increasePostCount,
